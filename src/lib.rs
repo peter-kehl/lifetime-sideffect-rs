a1f49d5230c2dde4860d2fc5d5f625aac4669faa
Peter Kehl <peter.kehl@gmail.com>
2024-06-12 19:34:56 -0700

    Comments

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,12 @@ pub fn caller_with_collection<
     own_items: Vec<OwnType>,
 ) {
     called_with_collection::<'_, '_, OwnType, OutType, OutCollectionType>(&own_items);
+    //                                                                    ^^^^^^^^^^
+    //                                                                    |
+    //                                                                    borrowed value does not live long enough
+    // argument requires that `own_items` is borrowed for `'out`
 }
+// - `own_items` dropped here while still borrowed
 
 pub fn called_with_collection<
     'own: 'out,
